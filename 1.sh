c90cd7e98a13151194f0ac1670cb4b41d385cda7
ssonawan <shrutisonawane0909@gmail.com>
2020-06-17 14:24:03 -0700

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get update
+
+sudo apt-get install nginx -y
+
+sudo ps auwx | grep nginx
+
+exit
+
+gcloud compute instances create gcelab2 --machine-type n1-standard-2 --zone us-central1-c
+
