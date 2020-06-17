b9d8223a4ad83c4306fd364483a418effe2cad3d
ssonawan <shrutisonawane0909@gmail.com>
2020-06-17 14:33:03 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,8 +2,6 @@ gcloud config set compute/zone us-central1-a
 
 gcloud container clusters create my-clus
 
-expect "NAME"
-
 gcloud container clusters get-credentials my-clus
 
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
@@ -12,3 +10,4 @@ kubectl expose deployment hello-server --type=LoadBalancer --port 8080
 
 kubectl get service
 
+gcloud container clusters delete my-clus
